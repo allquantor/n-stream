f74565a27e62a0170ba81d1881f943bf73110029
Ivan Morozov <ivan.morozov@kreditech.com>
2016-09-04 19:25:57 +0200

    Test are added

--- src/test/scala/de/kreditech/oktopus/prophet/CalculationServiceTest.scala
+++ src/test/scala/de/kreditech/oktopus/prophet/CalculationServiceTest.scala
@@ -1,6 +1,7 @@
 package de.kreditech.oktopus.prophet
 
 import akka.http.scaladsl.model.{ContentTypes, StatusCodes}
+import akka.http.scaladsl.server.{MalformedRequestContentRejection, UnsupportedRequestContentTypeRejection}
 import de.kreditech.oktopus.prophet.ProtocolTypes.ResourceResponse
 
 class CalculationServiceTest
@@ -14,7 +15,7 @@ class CalculationServiceTest
     "rows" : 5,
     "columns" : 100}""".stripMargin
 
-  "StatusService" when {
+  "Calculation Service " when {
     "POST /proposal" should {
       "return a proposal" in {
         Post("/proposal").withEntity(ContentTypes.`application/json`,
@@ -25,6 +26,29 @@ class CalculationServiceTest
       }
     }
   }
+
+  "Calculation Service " when {
+    "POST /proposal" should {
+      "rejectf wrong content type" in {
+        Post("/proposal").withEntity(ContentTypes.`text/csv(UTF-8)`,
+          requestJson) ~> standardRoute ~> check {
+          rejection shouldEqual  UnsupportedRequestContentTypeRejection(Set(ContentTypes.`application/json`))
+        }
+      }
+    }
+  }
+
+  "Calculation Service " when {
+    "POST /proposal" should {
+      "reject a request when json not valid" in {
+        Post("/proposal").withEntity(ContentTypes.`application/json`,
+          requestJson + "BREAK THE JSON!") ~> standardRoute ~> check {
+          rejection shouldBe a [MalformedRequestContentRejection]
+        }
+      }
+    }
+  }
+
 
 
 
